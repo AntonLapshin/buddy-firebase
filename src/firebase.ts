a5d2c8e612a6ec2fc5a86f4e68da8172e1531441
Anton Lapshin <antvallap@gmail.com>
2024-06-25 16:23:35 -0400

    Initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-5-J4DuxVQzNXRnehN8MtJkK0bEztrYM",
+  authDomain: "buddy-5e08b.firebaseapp.com",
+  projectId: "buddy-5e08b",
+  storageBucket: "buddy-5e08b.appspot.com",
+  messagingSenderId: "112307110644",
+  appId: "1:112307110644:web:1e77e2b807f407a37054a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
